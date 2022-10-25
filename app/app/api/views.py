7376f81dd88e0daeecb5bd73fffe245ec555bf65
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-25 12:04:46 +0300

    added setting for refresh token lifetime

--- app/app/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-
-
-def gerRoutes(request):
-    
-    routes = ['/api/token', '/api/token/refresh',]
-    return JsonResponse(routes, safe=False)
--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""auth URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,17 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework_jwt.views import obtain_jwt_token
 from django.contrib import admin
 from django.urls import path , include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.api/urls'))
- 
+    
+    path("api/", include('application.api.urls'))
 ]
